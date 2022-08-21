c38ad640f3d528d95d1420f9035003da209c46d1
胡赢 <ying-hu21@mails.tsinghua.edu.cn>
2022-08-21 12:21:47 +0800

    feat: add function guess_1 to read input feat: add enum Color

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+mod builtin_words;
+
 use console;
 use std::io::{self, Write};
+use std::io::ErrorKind::TimedOut;
+use crate::builtin_words::{FINAL, ACCEPTABLE};
 
 /// The main function for the Wordle game, implement your own logic here
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let is_tty = atty::is(atty::Stream::Stdout);
 
@@ -31,4 +36,49 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // TODO: parse the arguments in `args`
 
     Ok(())
+}
+
+fn match_result(word:String)->String{
+    // Calculate the color, and return a string of 5 letters
+    unimplemented!();
+}
+
+fn guess_1() -> Result<i32, Error> {
+    let mut n = 0;
+    let mut line = String::new();
+    io::stdin().read_line(&mut line)?;
+    let mut is_acceptable: bool = false;
+    for i in ACCEPTABLE {
+        if line == i.to_string() {
+            is_acceptable = true;
+            print!("{}",match_result(line));
+            break;
+        }
+    }
+    return if is_acceptable {
+        Ok(n)
+    } else {
+        Err(Error::InvalidWord)
+    }
+}
+
+enum Error {
+    InvalidWord
+}
+enum Color{
+    R,
+    Y,
+    G,
+    X
+}
+
+impl Color {
+    fn to_string(&self)->String{
+        return match self {
+            Color::R => {"R".to_string()}
+            Color::Y => {"Y".to_string()}
+            Color::G => {"G".to_string()}
+            Color::X => {"X".to_string()}
+        }
+    }
 }
